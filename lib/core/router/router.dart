ae17fe1d6655f917ae9c5b2ed3970ddfb695930b
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2024-01-30 23:40:36 +0900

    [이미지 검색앱] go_router 수정

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:image_search_day52/data/repository/pixabay_photo_repository.dart';
+import 'package:image_search_day52/domain/model/photo.dart';
+import 'package:image_search_day52/presentation/detail/detail_screen.dart';
 import 'package:image_search_day52/presentation/main/main_screen.dart';
 import 'package:image_search_day52/presentation/main/main_view_model.dart';
 import 'package:provider/provider.dart';
@@ -12,11 +14,22 @@ final GoRouter router = GoRouter(
       builder: (BuildContext context, GoRouterState state) {
         return ChangeNotifierProvider(
           create: (_) => MainViewModel(
-            GetTopFiveMostViewdImagesUseCase(),
+            GetTopFiveMostViewdImagesUseCase(
+              PixabayPhotoRepositoryImpl(),
+            ),
           ),
           child: const MainScreen(),
         );
       },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'details',
+          builder: (BuildContext context, GoRouterState state) {
+            final photo = state.extra as Photo;
+            return DetailScreen(photo: photo);
+          },
+        ),
+      ],
     ),
   ],
 );
